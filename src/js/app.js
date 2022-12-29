94515e5e41fb4d8f4b50f04a2be52ced2648e4f0
Hasan Hasanzade <101423091+hasan-hasanzade@users.noreply.github.com>
2022-12-29 18:51:45 +0400

    header
    started

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import * as flsFunctions from "./modules/functions.js";
+import './files/dynamic_adapt.js';
+import './files/regular.js';
+// import './files/sliders.js';
+import './files/functions.js';
+import './files/forms.js';
+import './files/script.js';
+
+flsFunctions.isWebp();
+
