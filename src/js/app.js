d49971358f93cab17cad04b911f141cee43e5250
Hasan Hasanzade <101423091+hasan-hasanzade@users.noreply.github.com>
2023-01-06 23:49:23 +0400

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import * as flsFunctions from "./modules/functions.js";
 import './files/dynamic_adapt.js';
 import './files/regular.js';
-// import './files/sliders.js';
+import './files/sliders.js';
 import './files/functions.js';
 import './files/forms.js';
 import './files/script.js';
